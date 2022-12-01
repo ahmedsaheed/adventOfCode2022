19f9f8356d46867c1521813e0d98ea936bafdf87
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-12-01 23:43:14 +0000

    day 01, part2

--- day001/day01.java
+++ day001/day01.java
@@ -2,7 +2,7 @@ import java.util.stream.*;
 import java.io.*;
 import java.util.*;
 class day01{
-       /// VERY BAD CODE !!! 
+       /// TERRIBLE CODE !!! 
     public Stream<String> readInput(String filename) throws IOException{
         BufferedReader br = new BufferedReader(new FileReader(filename));
         Stream<String> lines = br.lines();
@@ -12,7 +12,6 @@ class day01{
     public ArrayList<Integer> part1(Stream<String> lines){
         ArrayList<String> list = new ArrayList<String>();
         lines.forEach(list::add);
-        // System.out.println(list);
         ArrayList<Integer> newLineLocation = new ArrayList<Integer>();
         for(int i = 0; i < list.size(); i++){
             if(list.get(i).equals("")){
@@ -31,9 +30,7 @@ class day01{
            summer += Integer.parseInt(list.get(i));
         }
         sumOfEveryElf.add(summer);
-
         for (int i = newLineLocation.get(newLineLocation.size() - 2)  ; i < newLineLocation.get(newLineLocation.size() - 1)-1; i++){
-
            au += Integer.parseInt(list.get(i));
         }
         sumOfEveryElf.add(au);
@@ -41,8 +38,7 @@ class day01{
              for (int j=index; j < breakPoint - 1; j++){
                     sum += Integer.parseInt(list.get(j));
              }
-
-                     sumOfEveryElf.add(sum);
+            sumOfEveryElf.add(sum);
             sum = 0;
              iteration+=1;
              index = breakPoint;
@@ -53,34 +49,17 @@ class day01{
                 break;
             }
         }
-        // System.out.println(sumOfEveryElf);
-
-        System.out.println(Collections.max(sumOfEveryElf));
         return sumOfEveryElf;
     }
 
     public int getTopThreeElvs(ArrayList<Integer> list){
-        int first = 0; 
-        int second = 0; 
-        int third = 0;
-        first =   Collections.max(list);
-        for(int i= 0; i < list.size(); i++){
-            if (list.get(i) > second && list.get(i) != first){
-                second = list.get(i);
-            }
-        }
-
-        for(int i= 0; i < list.size(); i++){
-            if (list.get(i) > third  && list.get(i) != second && list.get(i) != first){
-                third = list.get(i);
-            }
-        }
-        return first + second + third;  
+       
+        Collections.sort(list);
+        return list.get(list.size()-1) + list.get(list.size() -2 ) +list.get(list.size() -3);  
     }
     public static void main(String[] args){
         day01 d = new day01();
         try{
-            Stream<String> lines = d.readInput("day01");
             System.out.println(d.part1(d.readInput("day01")));
             System.out.println(d.getTopThreeElvs(d.part1(d.readInput("day01"))));
         }catch(IOException e){
---
+++ day001/readme.md
@@ -0,0 +1,3 @@
+# Advent of Code 22
+
+This repo contains poor code, just what gets the stuff done.
